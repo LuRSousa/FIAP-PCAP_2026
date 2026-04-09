8b4d8909e1fcd3ec1358990bc264fb0ce7522457
LuRSousa <lucasrs.tech@gmail.com>
2026-04-09 10:53:54 -0300

    Testando bibliotecas (math e random)

---
+++ aula03-modulos-condicionais/aula03a-modulos.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+num = 17
+raiz = sqrt(num)
+print(f"A raíz de {num} é {raiz:.2f}")
+
+import random
+num_random = random.random()
+print(num_random * 10)
+
+num_rand_int = random.randint(1, 10)
+print(num_rand_int)
