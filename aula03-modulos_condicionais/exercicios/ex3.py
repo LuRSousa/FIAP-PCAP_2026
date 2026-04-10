4b01c946137c5dccd1709d6d9bdb76e14c6cac5f
LuRSousa <lucasrs.tech@gmail.com>
2026-04-10 10:35:16 -0300

    exercício 3: comparando dois números

---
+++ aula03-modulos_condicionais/exercicios/ex3.py
@@ -0,0 +1,14 @@
+#Faça um programa que peça dois números e imprima o maior deles, e informe caso eles sejam iguais
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+def comparando_nums(num1, num2):
+    if(num1 > num2):
+        print(f"{num1} é maior que {num2}.")
+    elif(num2 > num1):
+        print(f"{num2} é maior que {num1}.")
+    else:
+        print(f"{num1} e {num2} são iguais.")
+
+comparando_nums(num1, num2)
