29084561263b6d4a21e36d201c56d0abc3914e37
LuRSousa <lucasrs.tech@gmail.com>
2026-04-23 10:55:30 -0300

    exercício 1: mostrando número de 1 a n

---
+++ aula04-modulos_repeticao/exercicios/ex01.py
@@ -0,0 +1,9 @@
+#Programa que recebe um num inteiro positivo e imprime na tela todos os número de 1 a n
+
+n = int(input("Digite um número inteiro positivo: "))
+
+cont = 1
+
+while cont <= n:
+    print(cont)
+    cont += 1
