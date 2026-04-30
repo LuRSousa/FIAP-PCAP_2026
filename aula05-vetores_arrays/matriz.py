e991fa096c10442e9d8d265c4e9f71dcfb04028f
LuRSousa <lucasrs.tech@gmail.com>
2026-04-30 11:46:45 -0300

    testando matriz

---
+++ aula05-vetores_arrays/matriz.py
@@ -0,0 +1,14 @@
+musicas = [
+    ["Cha Cha Cha", "Bruno Mars"],
+    ["I Just Might", "Bruno Mars"],
+    ["Maluco Beleza", "Raul Seixas"],
+]
+
+print(musicas)
+
+print()
+
+for musica in musicas:
+    for info in musica:
+        print(info)
+    print()
