5478ba8c1c9ee306a4ffc2e6bbf530aeba283125
LuRSousa <lucasrs.tech@gmail.com>
2026-04-30 10:49:21 -0300

    testando for e for each com array

--- aula05-vetores_arrays/array.py
+++ aula05-vetores_arrays/array.py
@@ -4,3 +4,16 @@ print(lista_frutas[1])
 
 lista_frutas.append("Pera")
 print(lista_frutas)
+
+qnt_frutas = len(lista_frutas)
+print("Qnt de frutas: ", qnt_frutas)
+
+print()
+
+for i in range(qnt_frutas):
+    print(lista_frutas[i])
+
+print()
+
+for c in lista_frutas:
+    print(c)
